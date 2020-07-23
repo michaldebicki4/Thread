2d382226bc4ba44b3e3234f08a2e0e02c353e873
michaldebicki4 <michaldebicki4@gmail.com>
2020-07-23 14:28:12 +0200

    Initial commit

---
+++ src/com/michaldebicki/AnotherThread.java
@@ -0,0 +1,17 @@
+package com.michaldebicki;
+
+import static com.michaldebicki.ThreadColor.ANSI_BLUE;
+
+public class AnotherThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println(ANSI_BLUE + "Hello from " + currentThread().getName());
+
+            try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e){
+            System.out.println(ANSI_BLUE + "Another thread woke me up");
+        }
+        System.out.println(ANSI_BLUE + "Three seconds passed and I am awake ");
+    }
+}
---
+++ src/com/michaldebicki/Main.java
@@ -0,0 +1,40 @@
+package com.michaldebicki;
+
+import static com.michaldebicki.ThreadColor.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(ANSI_PURPLE + "Hello from the main thread");
+
+
+        Thread anotherThread = new AnotherThread();
+        anotherThread.setName("====AnotherThread=====");
+        anotherThread.start();
+
+        new Thread(){
+            public void run() {
+                System.out.println(ANSI_GREEN + "Hello from the anonymous class thread");
+            }
+        }.start();
+
+        Thread myRunnableThread = new Thread(new MyRunnable(){
+            @Override
+            public void run() {
+                System.out.println(ANSI_RED + "Hello from the anonymous class implementation of run");
+                try {
+                    anotherThread.join();
+                    System.out.println(ANSI_RED +"AnotherThread terminated, or timed out, so I am running again");
+                } catch (InterruptedException e){
+                    System.out.println(ANSI_RED + "I could not wait after all. I was interrupted");
+                }
+            }
+        });
+        myRunnableThread.start();
+        //anotherThread.interrupt();
+
+        System.out.println(ANSI_PURPLE + "Hello again from the main thread");
+
+
+    }
+}
